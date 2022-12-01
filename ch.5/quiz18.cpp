21ae427e841e848074c78c91dba8c3a38f83da5a
Gerard Di Mattia <gerard.dimattia@jjay.cuny.edu>
2022-12-01 09:44:26 -0500

    fixed quiz 18,19 and finished quiz 20.

--- ch.5/quiz18.cpp
+++ ch.5/quiz18.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
+bool Check(int a, int b, int c);
 
-int test(int a, int b, int c){
-    if (a+b==c)
-    cout<<"***TRUE***"<<a<<'+'<<b<<'='<<c<<endl;
+int main()
+{
+    int a,b,c;
+    int flag;
+    cout<<"Enter 3 integers: ";
+    cin>>a>>b>>c;
+    flag = Check(a,b,c);
+    if (flag == true)
+        cout<<"The sum of a and b is equal to c";
     else
-    cout<<"***FALSE***"<<a<<'+'<<b<<"!="<<c<<endl;
+        cout<<"the sum of a and b is not equal to c";
 
-    return c;
 }
 
-int main(){
-    cout<<test(1,3,3);
-    cout<<endl;
-   return 0;
+bool Check(int a, int b, int c)
+{
+    bool flag = true;
+    if (a + b != c)
+        flag = false;
+    
+    return flag;
 }
--- ch.5/quiz19.cpp
+++ ch.5/quiz19.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
+bool isPrime(int num);
 
-
-
-
-int Prime(int x)
-{
-    if (x%2 == 0 && x!=2)
-    cout<<"The input is not a Prime: ";
-    else
-    cout<<"The input is a Prime: ";
-    return x;
+int main(){
+   int num;
+   bool flag;
+   cout<<"Enter any number(should be positive integer): ";
+   cin>>num;
+   flag = isPrime(num);
+   if (flag==true)
+      cout<<num<<" is a prime number";
+   else
+      cout<<num<<" is not a prime number";
+   return 0;
 }
-
-
-int main()
-{
-    int y;
-    cout<<"Enter an integer: ";
-    cin>>y;
-    cout<<Prime(y);
-
-    cout<<endl;
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) 
+    {
+       if(num % i == 0) 
+       {
+          flag = false;
+          break;
+       }
+    }
+    return flag;
 }
-
-
